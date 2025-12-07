d8c42dd7b48424552f7ee47b545a939b3d0811b1
Juunibyou <danijunn@gmail.com>
2025-12-07 01:20:33 -0600

    update

--- app/src/main/java/org/example/Dijkstra.java
+++ app/src/main/java/org/example/Dijkstra.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import java.util.*;
+
 public class Dijkstra {
-    public static int shortestPath(){
+
+    public static int shortestPath(Graph graph, int start, int end) {
+        Map <Integer, Integer> distances = new HashMap<>();
         
+        for (int n : graph.getNodes()) {
+            distances.put(n, Integer.MAX_VALUE);
+        }
+
+        distances.put(start, 0);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            int currentId = current.getId();
+            int currentDist = current.getDistance();
+
+            if (currentDist > distances.get(currentId)) {
+                continue;
+            }
+
+            for (Edge edge : graph.getNexts(currentId)) {
+                int neighbor = edge.getTo();
+                int newDist = currentDist + edge.getWeight();
+
+                if (newDist < distances.get(neighbor)) {
+                    distances.put(neighbor, newDist);
+                    pq.add(new Node(neighbor, newDist));
+                }
+            }
+        }
 
         return -1;
     }
